7f7a062ffce25cb75d3befd43c504febe72cd243
sweisscct <114678287+sweisscct@users.noreply.github.com>
2026-05-14 15:23:01 +0100

    Fetch Image and Repository Access Issue Resolution

--- promise.js
+++ promise.js
@@ -45,13 +45,60 @@ latitude=53.3331
 let weatherData = fetch("https://api.open-meteo.com/v1/forecast?latitude=53.3331&longitude=-6.2489&hourly=temperature_2m,rain");
 // console.log(weatherData);
 
-weatherData
-.then(response => {
-    // HTTP Response
-    // console.log(response);
+// weatherData
+// .then(response => {
+//     // HTTP Response
+//     // console.log(response);
 
-    // console.log(response.json())
-    response.json().then(processedData => {
-        console.log(processedData);
-    })
-})
+//     // console.log(response.json())
+//     response.json().then(processedData => {
+//         // console.log(processedData);
+//         const hourlyData = processedData.hourly;
+//         const timeData = hourlyData.time;
+//         const tempData = hourlyData.temperature_2m;
+//         const rainData = hourlyData.rain;
+//         for (let time=0; time<24; time++) {
+//             console.log(`Time: ${timeData[time]}, temp: ${tempData[time]}, rain: ${rainData[time]} `);
+//         }
+//     })
+// })
+
+// handles resolves
+async function getWeatherData() {
+    let weatherData = await fetch("https://api.open-meteo.com/v1/forecast?latitude=53.3331&longitude=-6.2489&hourly=temperature_2m,rain");
+    let processedData = await weatherData.json();
+    const hourlyData = processedData.hourly;
+    const timeData = hourlyData.time;
+    const tempData = hourlyData.temperature_2m;
+    const rainData = hourlyData.rain;
+    for (let time=0; time<24; time++) {
+        console.log(`Time: ${timeData[time]}, temp: ${tempData[time]}, rain: ${rainData[time]} `);
+    }    
+}
+
+getWeatherData();
+
+async function getImageURI(url) {
+    const image = document.getElementById("cool-image");
+    let imageData = await fetch(url);
+    let processedData = await imageData.blob();
+    image.src = await URL.createObjectURL(processedData);
+}
+
+
+ getImageURI("https://i0.wp.com/www.sara-sees.com/wp-content/uploads/2016/04/killiney2-e1461493910573.jpg?w=1716");
+
+
+
+
+
+
+
+/*
+
+Repository Access Issue
+
+You no longer have access to your assignment repository. Contact your teacher for support.
+
+
+*/
